99771b5c5b68ec7d9e6a59f0babe54a3a2d13cfb
jlswbs <jlswbs@users.noreply.github.com>
2023-03-27 11:06:45 +0200

    Add files via upload

---
+++ Eye_Candy/Eye_Candy.ino
@@ -0,0 +1,62 @@
+// Eye candy patterns //
+
+#include "M5AtomS3.h"
+M5Display Lcd;
+
+#define WIDTH   130
+#define HEIGHT  130
+#define SCR     (WIDTH*HEIGHT)
+#define NUMS    24
+#define COLS    12
+
+float randomf(float minf, float maxf) {return minf + (esp_random()%(1UL << 31))*(maxf - minf) / (1UL << 31);}
+
+  uint16_t col[SCR];
+  float p[SCR];
+  float v[SCR];
+  float a[SCR];
+  uint16_t color[NUMS];
+
+void rndrule(){
+  
+  memset(col, 0, 2*SCR);
+  memset(p, 0, 4*SCR);
+  memset(v, 0, 4*SCR);
+  memset(a, 0, 4*SCR);
+
+  for (int i = 0; i < NUMS; i++) v[(1+esp_random()%(WIDTH-2))+(1+esp_random()%(HEIGHT-2))*WIDTH] = randomf(0.0f, TWO_PI);
+  for (int i = 0; i < COLS; i++) color[i] = esp_random();
+         
+}
+
+void setup(void){
+
+  srand(time(NULL));
+
+  M5.begin();
+  
+  rndrule();
+
+}
+
+void loop(void){
+
+  if (M5.Btn.wasReleased()) rndrule();
+  
+  for (int y = 1; y < HEIGHT-1; y++) {
+    for (int x = 1; x < WIDTH-1; x++) {
+      a[x+y*WIDTH] = (v[(x-1)+y*WIDTH] + v[(x+1)+y*WIDTH] + v[x+(y-1)*WIDTH] + v[x+(y+1)*WIDTH]) * 0.25f - v[x+y*WIDTH];
+    }
+  }
+  
+  for (int i = 0; i < SCR; i++){
+    v[i] += a[i];
+    p[i] += v[i];
+    uint8_t coll = COLS * sinf(p[i]);
+    col[i] = color[coll%COLS];
+  }
+
+  Lcd.drawBitmap(0, 0, WIDTH, HEIGHT, col);
+  M5.update();
+
+}
